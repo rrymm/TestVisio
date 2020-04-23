a1c6ceec12e787ab7119d1fc9d88c748ee0f9330
Damien Le Dantec <damien.ledantec@telecomsante.com>
2020-04-23 19:09:25 +0200

    Rename project name for ios

--- ios/MyHoppenVisioTests/ExempleTests.m
+++ ios/MyHoppenVisioTests/ExempleTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ExempleTests : XCTestCase
+@interface MyHoppenVisioTests : XCTestCase
 
 @end
 
-@implementation ExempleTests
+@implementation MyHoppenVisioTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
